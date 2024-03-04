c9a49e5284a7c63ac1d6aba8189a8c09a074108e
npx <priyanka@lyrostech.com>
2024-03-05 00:34:26 +0530

    yes

--- src/Estorebody.js
+++ src/Estorebody.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import img1 from '.images/img1.jpg';
-// import img2 from './img2.jpg';
-// import img3 from './img3.jpg';
-// import img4 from './img4.jpg';
-// import img5 from './img5.jpg';
-// import img6 from './img6.jpg';
+import img1 from '.images/img1.jpg';
+import img2 from './img2.jpg';
+import img3 from './img3.jpg';
+import img4 from './img4.jpg';
+import img5 from './img5.jpg';
+import img6 from './img6.jpg';
 export default function Estorebody(){
 const products = [
   {
