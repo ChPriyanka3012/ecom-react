29da48328a6847be8caeb281eb4246e95240775a
npx <priyanka@lyrostech.com>
2024-03-05 14:19:28 +0530

    a

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import './App.css';
 import Estoreheader from './Estoreheader';
 import Estorebody from './Estorebody';
+import Block from './Block';
 //import Block from './Block';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Estoreheader />
-    <Estorebody />
+    <Block />
     
     
     
